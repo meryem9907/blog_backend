7973c1cff505009d3a23fa547712c6fdc6403452
meryem9907 <meryem9907@googlemail.com>
2025-10-27 16:12:41 +0100

    Add dockerfile

--- blog_rest_backend/settings.py
+++ blog_rest_backend/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ovqgc-%hx^$@xcg!4t&3g-qgir*$&se!me#27=ueaq7f575z-%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS","127.0.0.1").split(",")
 # in case of a browser request set this correctly
 CORS_ALLOWED_ORIGINS = ["https://*"]
 
@@ -108,6 +107,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        "OPTIONS": {"timeout": 20}
     }
 }
 
