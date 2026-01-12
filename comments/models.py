fc4ae5928492746b71ab5b3489dc4501fc7a72fa
meryem9907 <meryem9907@googlemail.com>
2026-01-12 13:54:02 +0100

    Migrate and Delete unused imports

--- comments/models.py
+++ comments/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.core.exceptions import ValidationError
 
 from posts.models import Post
-from users.models import Author
 
 class Comment(models.Model):
     body = models.TextField()
